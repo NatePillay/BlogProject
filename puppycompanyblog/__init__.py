ff81fcfff7340fc33e69c36eaeb420815d864468
NatePillay <nathan94pillay@gmail.com>
2023-05-29 22:45:52 +0200

    new commit

--- puppycompanyblog/__init__.py
+++ puppycompanyblog/__init__.py
@@ -1,6 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+
 from puppycompanyblog.core.views import core
+from puppycompanyblog.error_pages.handlers import error_pages
+
+
+#linking views
 app.register_blueprint(core)
+app.register_blueprint(error_pages)
 
